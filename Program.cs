6ef11907183dc0811c30735709853b44d33bcb6f
rfiieka <126036603+rfiieka@users.noreply.github.com>
2023-02-22 12:43:47 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using v21;
+
+Kalkulator hitung = new Kalkulator();
+
+//hitung.tambah();
+//hitung.kurang();
+
+Console.Write("Masukkan panjang : ");
+int p = Convert.ToInt32(Console.ReadLine());
+Console.Write("Masukkan lebar : ");
+int l = Convert.ToInt32(Console.ReadLine());
+
+Rumus rumuspersegi = new Rumus();
+ 
+rumuspersegi.lingkaran();
+
+Console.WriteLine($"luas persegi panjang dengan L ={p} dan L = {l} adalah"+ rumuspersegi.PersegiPanjang(p, l));
---
+++ v21.cs
@@ -0,0 +1,26 @@
+namespace v21
+{
+
+    class Kalkulator
+    {
+        public void tambah(){
+            Console.WriteLine("tambah");
+        }
+
+        public void kurang(){
+            Console.WriteLine("kurang");
+        }
+    }
+
+    class Rumus 
+    {
+        
+        public void lingkaran(){
+            Console.WriteLine("lingkaran");
+        }
+
+        public int PersegiPanjang(int p, int l ){
+            return p*l;
+        }
+    }
+}
